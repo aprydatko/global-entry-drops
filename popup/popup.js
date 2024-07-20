50af6f311e86c8c410252ebf2b7c90d1977c474a
Придатко Артур Анатолійович <artur.pridatko@a-bank.com.ua>
2024-07-20 16:52:12 +0300

    add validation

--- popup/popup.js
+++ popup/popup.js
@@ -7,19 +7,102 @@ const endDateElement = document.getElementById('endDate')
 const startButton = document.getElementById('startButton')
 const stopButton = document.getElementById('stopButton')
 
-startButton.onclick = () => {
-	const prefs = {
-		locationId: locationIdElement.value,
-		startDate: startDateElement.value,
-		endDate: endDateElement.value,
-		tzData: locationIdElement.options[
-			locationIdElement.selectedIndex
-		].getAttribute('data-tz'),
+// Span listeners
+const runningSpan = document.getElementById('runningSpan')
+const stoppedSpan = document.getElementById('stoppedSpan')
+
+// Error message
+const locationError = document.getElementById('locationError')
+const startDateError = document.getElementById('startDateError')
+const endDateError = document.getElementById('endDateError')
+
+const hideElement = (elem) => {
+	elem.style.display = 'none'
+}
+
+const showElement = (elem) => {
+	elem.style.display = ''
+}
+
+const disableElement = (elem) => {
+	elem.disabled = true
+}
+
+const enableElement = (elem) => {
+	elem.disabled = false
+}
+
+const handleOnStartState = () => {
+	// Spans
+	showElement(runningSpan)
+	hideElement(stoppedSpan)
+	// Buttons
+	disableElement(startButton)
+	enableElement(stopButton)
+	// Inputs
+	disableElement(locationIdElement)
+	disableElement(startDateElement)
+	disableElement(endDateElement)
+}
+
+const handleOnStopState = () => {
+	// Spans
+	showElement(stoppedSpan)
+	hideElement(runningSpan)
+	// Buttons
+	disableElement(stopButton)
+	enableElement(startButton)
+	// Inputs
+	enableElement(locationIdElement)
+	enableElement(startDateElement)
+	enableElement(endDateElement)
+}
+
+const performOnStartValidations = () => {
+	if (!locationIdElement.value) {
+		showElement(locationError)
+	} else {
+		hideElement(locationError)
 	}
-	chrome.runtime.sendMessage({ event: 'onStart', prefs })
+
+	if (!startDateElement.value) {
+		showElement(startDateError)
+	} else {
+		hideElement(startDateError)
+	}
+
+	if (!endDateElement.value) {
+		showElement(endDateError)
+	} else {
+		hideElement(endDateError)
+	}
+
+	return (
+		locationIdElement.value &&
+		startDateElement.value &&
+		endDateElement.value
+	)
+}
+
+startButton.onclick = () => {
+	const allFieldsValid = performOnStartValidations()
+
+	if (allFieldsValid) {
+		handleOnStartState()
+		const prefs = {
+			locationId: locationIdElement.value,
+			startDate: startDateElement.value,
+			endDate: endDateElement.value,
+			tzData: locationIdElement.options[
+				locationIdElement.selectedIndex
+			].getAttribute('data-tz'),
+		}
+		chrome.runtime.sendMessage({ event: 'onStart', prefs })
+	}
 }
 
 stopButton.onclick = () => {
+	handleOnStopState()
 	chrome.runtime.sendMessage({ event: 'onStop' })
 }
 
@@ -42,7 +125,13 @@ chrome.storage.local.get(
 			endDateElement.value = endDate
 		}
 
-		console.log('Running Status', isRunning)
+		console.log('isRunning', isRunning)
+
+		if (isRunning) {
+			handleOnStartState()
+		} else {
+			handleOnStopState()
+		}
 	}
 )
 
